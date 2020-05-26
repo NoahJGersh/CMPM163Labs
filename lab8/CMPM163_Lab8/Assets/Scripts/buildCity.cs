a2aa4536446056dadd8316b70cf3aeca6282a760
ngersh <ngersh@ucsc.edu>
2020-05-26 15:57:58 -0700

    Finalize initial procedural city.

--- lab8/CMPM163_Lab8/Assets/Scripts/buildCity.cs
+++ lab8/CMPM163_Lab8/Assets/Scripts/buildCity.cs
@@ -9,25 +9,33 @@ public class buildCity : MonoBehaviour
     public int mapWidth = 20;
     public int mapHeight = 20;
     public int suburbRange = 5;
+    public float noiseFactor = 10.0f;
+    public int noiseSeed = 555;
 
     int buildingFootprint = 10;
+
+    private int noiseBound;
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int h = 0; h <= mapHeight; ++h)
+        noiseBound = buildings.Length - 1;
+        if (noiseSeed == -1) noiseSeed = Random.Range(0, 3000);
+
+        for (int h = 0; h < mapHeight; ++h)
         {
-            for (int w = 0; w <= mapWidth; ++w)
+            for (int w = 0; w < mapWidth; ++w)
             {
-                bool isSkyScraper = ((h >= suburbRange && h <= (mapHeight - suburbRange)) && (w >= suburbRange && w <= (mapWidth - suburbRange)));
-                float minScale = isSkyScraper ? 0.75f : 0.3f;
-                float maxScale = isSkyScraper ? 1.5f : 0.5f;
-                float scale = Random.Range(minScale, maxScale);
+                int noiseResult = (int)(Mathf.PerlinNoise(w / noiseFactor + noiseSeed, h / noiseFactor + noiseSeed) * noiseBound);
                 Vector3 pos = new Vector3(w * buildingFootprint, 0, h * buildingFootprint);
 
-                int n = Random.Range(0, buildings.Length);
-                GameObject curBuilding = Instantiate(buildings[n], pos, Quaternion.identity);
-                curBuilding.transform.localScale = new Vector3(1.0f, scale, 1.0f);
+                //bool isSkyScraper = ((h >= suburbRange && h <= (mapHeight - suburbRange)) && (w >= suburbRange && w <= (mapWidth - suburbRange)));
+                //float minScale = isSkyScraper ? 0.75f : 0.3f;
+                //float maxScale = isSkyScraper ? 1.5f : 0.5f;
+                int buildingType = Mathf.FloorToInt(noiseResult);
+
+                GameObject curBuilding = Instantiate(buildings[buildingType], pos, Quaternion.identity);
+                curBuilding.transform.localScale = new Vector3(1.0f, 0.3f * (buildingType + 1), 1.0f);
             }
         }
         
