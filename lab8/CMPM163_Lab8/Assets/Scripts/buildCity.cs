9246e6081a00a2e8c187fb994c0ae366bb3b95b4
ngersh <ngersh@ucsc.edu>
2020-05-26 16:38:19 -0700

    Adjust prefab meshes and materials to fit intended aesthetic.

--- lab8/CMPM163_Lab8/Assets/Scripts/buildCity.cs
+++ lab8/CMPM163_Lab8/Assets/Scripts/buildCity.cs
@@ -8,7 +8,7 @@ public class buildCity : MonoBehaviour
     public GameObject[] buildings;
     public int mapWidth = 20;
     public int mapHeight = 20;
-    public int suburbRange = 5;
+    //public int suburbRange = 5;
     public float noiseFactor = 10.0f;
     public int noiseSeed = 555;
 
@@ -19,7 +19,7 @@ public class buildCity : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        noiseBound = buildings.Length - 1;
+        noiseBound = buildings.Length;
         if (noiseSeed == -1) noiseSeed = Random.Range(0, 3000);
 
         for (int h = 0; h < mapHeight; ++h)
@@ -32,18 +32,21 @@ public class buildCity : MonoBehaviour
                 //bool isSkyScraper = ((h >= suburbRange && h <= (mapHeight - suburbRange)) && (w >= suburbRange && w <= (mapWidth - suburbRange)));
                 //float minScale = isSkyScraper ? 0.75f : 0.3f;
                 //float maxScale = isSkyScraper ? 1.5f : 0.5f;
-                int buildingType = Mathf.FloorToInt(noiseResult);
+                int buildingType = Mathf.Min(Mathf.FloorToInt(noiseResult), noiseBound - 1);
 
                 GameObject curBuilding = Instantiate(buildings[buildingType], pos, Quaternion.identity);
                 curBuilding.transform.localScale = new Vector3(1.0f, 0.3f * (buildingType + 1), 1.0f);
+
+                int n = Mathf.FloorToInt(Random.Range(0, 3.99f));
+                curBuilding.transform.Rotate(new Vector3(0.0f, 90 * n, 0.0f));
             }
         }
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
